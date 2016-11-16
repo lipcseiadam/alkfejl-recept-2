4ec90767822e2c8963cc1d5240d762c08d006166
Lipcsei Ádám <shifty@INF.ELTE.HU>
2016-11-16 15:32:09 +0100

    káosz^^

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -15,5 +15,10 @@ const $panels = $('.panel')
 $panels.each(function (){
     const $panel = $(this)
     const db = $panel.find('.list-group-item').length
-    const $label = $('<span>')
+    $panel.find('.panel-heading span').before('(${db})')
+})
+
+$headings = $('.panel-heading')
+$headings.on('click', function(e){
+    console.log(this)
 })
